fb49df5181dd047a21de59248ce5106b814e0a7d
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-27 13:32:39 +0300

    feat: add table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,31 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 7, b: 2, action: Action.Subtract, expected: 5 },
+  { a: 19, b: 10, action: Action.Subtract, expected: 9 },
+  { a: 32, b: 7, action: Action.Subtract, expected: 25 },
+  { a: 7, b: 2, action: Action.Multiply, expected: 14 },
+  { a: 19, b: 10, action: Action.Multiply, expected: 190 },
+  { a: 3, b: 7, action: Action.Multiply, expected: 21 },
+  { a: 8, b: 2, action: Action.Divide, expected: 4 },
+  { a: 121, b: 11, action: Action.Divide, expected: 11 },
+  { a: 700, b: 7, action: Action.Divide, expected: 100 },
+  { a: 8, b: 2, action: Action.Exponentiate, expected: 64 },
+  { a: 11, b: 2, action: Action.Exponentiate, expected: 121 },
+  { a: 10, b: 3, action: Action.Exponentiate, expected: 1000 },
+  { a: 10, b: 3, action: 'unknown action', expected: null },
+  { a: '10', b: '3', action: Action.Divide, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'validateEmail(%s) should be %s',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toEqual(expected);
+    },
+  );
 });
