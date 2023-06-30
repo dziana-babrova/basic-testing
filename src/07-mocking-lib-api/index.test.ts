c8bcf9ce433d5bf4eefac6eba48108f1a9a6a88c
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-30 15:11:13 +0300

    feat: add test with mocking lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,87 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+    jest.resetAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const spy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi('/users');
+    jest.advanceTimersByTime(5000);
+    expect(spy).toBeCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const spy = jest.spyOn(axios.Axios.prototype, 'get');
+    await throttledGetDataFromApi('/users');
+    jest.advanceTimersByTime(5000);
+    expect(spy).toHaveBeenCalledWith('/users');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const data = [
+      {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        address: {
+          street: 'Kulas Light',
+          suite: 'Apt. 556',
+          city: 'Gwenborough',
+          zipcode: '92998-3874',
+          geo: {
+            lat: '-37.3159',
+            lng: '81.1496',
+          },
+        },
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+        company: {
+          name: 'Romaguera-Crona',
+          catchPhrase: 'Multi-layered client-server neural-net',
+          bs: 'harness real-time e-markets',
+        },
+      },
+      {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        address: {
+          street: 'Victor Plains',
+          suite: 'Suite 879',
+          city: 'Wisokyburgh',
+          zipcode: '90566-7771',
+          geo: {
+            lat: '-43.9509',
+            lng: '-34.4618',
+          },
+        },
+        phone: '010-692-6593 x09125',
+        website: 'anastasia.net',
+        company: {
+          name: 'Deckow-Crist',
+          catchPhrase: 'Proactive didactic contingency',
+          bs: 'synergize scalable supply-chains',
+        },
+      },
+    ];
+    jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockImplementationOnce(async () => Promise.resolve({ data }));
+    const result = await throttledGetDataFromApi('/users');
+    jest.advanceTimersByTime(5000);
+    expect(result).toBe(data);
   });
 });
